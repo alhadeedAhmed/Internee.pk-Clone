302019e8550022de20d2bae2418eeaf129d5e05b
ALHADEED SUDHEER <92500828+alhadeedAhmed@users.noreply.github.com>
2024-07-27 08:52:02 +0500

    Update ArrowButton.jsx

--- internee-clone/src/components/ArrowButton/ArrowButton.jsx
+++ internee-clone/src/components/ArrowButton/ArrowButton.jsx
@@ -3,18 +3,16 @@ import React from "react";
 import "./ArrowButton.css";
 
 const ArrowButton = () => {
-  // Function to scroll to the top of the page
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth", // Smooth scrolling
+      behavior: "smooth", 
     });
   };
 
   return (
     <div className="arrow-button" onClick={scrollToTop}>
       <i className="fa fa-arrow-up" aria-hidden="true"></i>{" "}
-      {/* Up arrow icon */}
     </div>
   );
 };
